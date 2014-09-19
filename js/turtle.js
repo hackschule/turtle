326460be0c7953d1d5285fbd8ad9afb98f04495f
Michael Specht <micha.specht@gmail.com>
2014-09-19 14:42:17 +0200

    re-arranged directory structure

--- js/turtle.js
+++ js/turtle.js
@@ -76,3 +76,28 @@ function setpos(x, y, phi)
     window.turtle_y = y;
     window.turtle_phi = phi * Math.PI / 180.0;
 }
+
+function loadScript(url, callback)
+{
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.onreadystatechange = callback;
+    script.onload = callback;
+    head.appendChild(script);
+}
+
+loadScript('js/jquery-1.7.1.min.js', function() {
+    $('body').css('padding', '0');
+    $('body').css('margin', '0');
+    $('body').css('background-color', '#000');
+    $('body').css('text-align', 'center');
+
+    $('body').append("<canvas id='canvas' width='700' height='700'>");
+    initCanvas();
+    window.onerror = function(msg, url, line) {
+        console.log(msg, url, line);
+    };
+    main();
+});
